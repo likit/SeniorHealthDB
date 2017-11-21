6a3a263241bd964e6eb2d8e31d7b433ef6d6cd51
Likit Preeyanon <likit.pre@mahidol.edu>
2017-11-21 15:36:41 +0700

    updated logout view
    login view redirected to main form

--- webview/auth/views.py
+++ webview/auth/views.py
@@ -1,5 +1,5 @@
-from flask_login import login_user, current_user, logout_user
-from flask import request, render_template, redirect
+from flask_login import login_user, current_user, logout_user, login_required
+from flask import request, render_template, redirect, url_for
 from . import auth_blueprint as auth
 from server import db, UserQuery
 from models import User
@@ -7,6 +7,9 @@ from models import User
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+
     pid = request.form.get('pid', None)
     passcode = request.form.get('passcode', None)
     if not pid or not passcode:
@@ -21,11 +24,14 @@ def login():
         return render_template('auth/loginerror.html')
     else:
         login_user(u)
-        return render_template('auth/login.html')
+        return redirect(url_for('form.main'))
 
 
 @auth.route('/logout')
+@login_required
 def logout():
+    if 'form_data' in session:
+        del session['form_data']
     logout_user()
     return redirect('/')
 
